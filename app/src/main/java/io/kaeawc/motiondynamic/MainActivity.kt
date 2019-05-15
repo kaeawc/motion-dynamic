ad332a5e9c894a90f05f9c6fe453e82257956e3f
Jason Pearson <jason.d.pearson@gmail.com>
2019-05-15 12:20:39 -0400

    use setMargin with updateState

--- app/src/main/java/io/kaeawc/motiondynamic/MainActivity.kt
+++ app/src/main/java/io/kaeawc/motiondynamic/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onMotion(@Suppress("UNUSED_ARGUMENT") view: View) {
-        Timber.d("onMotion")
 
         when (motion_scene?.currentState) {
             R.id.start -> onStartMotion()
@@ -35,38 +34,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onStartMotion() {
-        Timber.d("onStartMotion")
 
-//        val newConstraints = reconstrain(motion_scene?.getConstraintSet(R.id.start), R.id.square)
+        reconstrain(motion_scene?.getConstraintSet(R.id.start), R.id.square)
 
-
-        val cs = motion_scene?.getConstraintSet(R.id.start)
-        val parent = R.id.motion_scene
-        val size = resources.getDimensionPixelSize(R.dimen.object_size)
-        val range = resources.displayMetrics.heightPixels - size
-        val topMargin = Random.nextInt(range)
-        Timber.i("topMargin: $topMargin")
-
-//        cs?.setHorizontalBias(R.id.square, Random.nextFloat());
-        cs?.setVerticalBias(R.id.square, Random.nextFloat());
-        cs?.clear(R.id.square, TOP)
-        cs?.clear(R.id.square, BOTTOM)
-        cs?.setMargin(R.id.square, START, Random.nextInt(200))
-
-        motion_scene?.updateState(R.id.start, cs)
-
-//        resetState(R.id.start)
-
-//        motion_scene?.rebuildScene()
-
-//        motion_scene?.setTransition(R.id.start, R.id.end)
         motion_scene?.transitionToEnd()
-
-
     }
 
     private fun onEndMotion() {
-        Timber.d("onEndMotion")
 
         motion_scene?.apply {
             motion_scene?.transitionToStart()
@@ -74,21 +48,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resetState(state: Int) {
-        Timber.d("resetState")
         val widthPixels = resources.displayMetrics.widthPixels
         val heightPixels = resources.displayMetrics.heightPixels
         motion_scene?.setState(state, widthPixels, heightPixels)
     }
 
-    private fun reconstrain(constraintSet: ConstraintSet?, view: Int): ConstraintSet? {
-        Timber.d("reconstrain")
+    private fun reconstrain(constraintSet: ConstraintSet?, view: Int) {
         val parent = R.id.motion_scene
         val size = resources.getDimensionPixelSize(R.dimen.object_size)
         val range = resources.displayMetrics.heightPixels - size
         val topMargin = Random.nextInt(range)
-        Timber.i("topMargin: $topMargin")
-
-        constraintSet?.connect(view, TOP, parent, TOP, topMargin)
-        return constraintSet
+        constraintSet?.setMargin(view, TOP, topMargin)
+        motion_scene?.updateState(R.id.start, constraintSet)
     }
 }
